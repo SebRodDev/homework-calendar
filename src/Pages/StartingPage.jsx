e4c2b75b617a412df2d650cefb8741cbd95d1649
Sebastian Rodriguez <sebastianrodriguez221@hotmail.com>
2024-12-15 21:10:33 -0500

    More progress on my project

--- src/Pages/StartingPage.jsx
+++ src/Pages/StartingPage.jsx
@@ -3,9 +3,14 @@ import './StartingPageLooks.css'
 
 const StartingPage = () => {
   return (
-    <div>
-      <h1>Homework Calendar</h1>
-      <h2>A place where you can create a list of all your homework assignments!</h2>
+    <div class="background">
+      <div class="background">
+      <h1 class="titleText">Homework Calendar</h1>
+      <h2 class="titleText">A place where you can create a list of all your homework assignments!</h2>
+
+      <button class="logoutButton">logout</button>
+      </div>
+    
     </div>
   );
 };
