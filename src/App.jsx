1239a8dfc750bed25f5972a79c9aa86c449980a3
Sebastian Rodriguez <sebastianrodriguez221@hotmail.com>
2024-12-15 19:48:37 -0500

    More Progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Routes, Route} from 'react-router-dom'
+import StartingPage from './Pages/StartingPage';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
     <Routes>
-
-
-
-      
+      <Route path="/" element={<StartingPage/>}/>
     </Routes>
     
     
     
-    </>
+    </BrowserRouter>
 
 
   );
---
+++ src/Pages/StartingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './StartingPageLooks.css'
+
+const StartingPage = () => {
+  return (
+    <div>
+      <h1>Homework Calendar</h1>
+      <h2>A place where you can create a list of all your homework assignments!</h2>
+    </div>
+  );
+};
+
+export default StartingPage;
