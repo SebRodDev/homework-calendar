a47a013c5a8cf01a173f0f8f4e6daa5bbb8232e1
Sebastian Rodriguez <sebastianrodriguez221@hotmail.com>
2024-12-17 14:30:08 -0500

    Stuff

--- src/Pages/StartingPage.jsx
+++ src/Pages/StartingPage.jsx
@@ -3,13 +3,23 @@ import './StartingPageLooks.css'
 
 const StartingPage = () => {
   return (
-    <div>
+    <div class="background">
+      <header>
+        <nav>
+        <ul class = "navLinks">
+        <li><a href = "#">Login</a></li>
+        <li><a href = "#">Create Account</a></li>
+        <li><a href = "#">About</a></li>
+        </ul>
+        </nav>
+        </header>
       <div className="background">
         <div className="plainBackground">
         <h1 className="titleText plainBackground">Homework Calendar</h1>
         <h2 className="titleText">A place where you can create a list of all your homework assignments!</h2>
         </div>
-      <button className="logoutButton">logout</button>
+      
+      
       </div>
     
     </div>
