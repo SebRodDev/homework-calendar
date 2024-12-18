204de0f4290127c622daf6804edf3f686fb4e3a4
Sebastian Rodriguez <sebastianrodriguez221@hotmail.com>
2024-12-17 23:35:47 -0500

    Further progress on the frontend of my project

--- src/Pages/StartingPage.jsx
+++ src/Pages/StartingPage.jsx
@@ -5,10 +5,11 @@ import { Rainify } from 'rainify';
 const StartingPage = () => {
   return (
     <div>
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css'></link>
       <div class="background">
       <Rainify 
         isRaining 
-        intensity={200}
+        intensity={125}
         color='rgba(255, 255, 255, 0.5)'
         zIndex={0}
         speed={10}
@@ -30,8 +31,6 @@ const StartingPage = () => {
       <div> 
       <h1 className="titleText">Homework Calendar</h1>
       <h2 className="subText">A place where you can create a list of all your homework assignments!</h2>
-      
-      
       </div>
 
       </div>
