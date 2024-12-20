773e5ecdafea8b5089c4a9c797813c866886b345
Sebastian Rodriguez <sebastianrodriguez221@hotmail.com>
2024-12-19 20:34:20 -0500

    Adding styles

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import './Styles/LoginLooks.css';
+import { Rainify } from 'rainify';
 
 export default function Login() {
     return (
-        <div>
+        <div class="loginBackground">
+            <Rainify /* Just the rain effects from the library that I imported */
+                    isRaining 
+                    intensity={125}
+                    color='rgba(255, 255, 255, 0.5)'
+                    zIndex={0}
+                    speed={10}
+                    thickness={1}
+                    splashColor='rgba(255, 255, 255, 0.5)'
+                    splashDuration={0}>
+                  </Rainify>
+                  
             <div class="loginSection">
             <h1>Login</h1>
             <h2>Please enter your information</h2>
