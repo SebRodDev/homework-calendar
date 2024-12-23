03aa54e984ed5e47ade8e1b62cd5b9e05fad1819
Sebastian Rodriguez <sebastianrodriguez221@hotmail.com>
2024-12-22 19:54:15 -0500

    Finished the create account page

--- src/Pages/CreateAccountPage.jsx
+++ src/Pages/CreateAccountPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Rainify } from 'rainify';
-import './Styles/CreateAccountPageLooks.css'
+import './Styles/CreateAccountPageLooks.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreateAccountPage() {
+    let navigate = useNavigate();
     return (
         <div class="createBackground">
             <Rainify 
@@ -16,10 +18,15 @@ export default function CreateAccountPage() {
             splashDuration={0}>
             </Rainify>
 
+            <button type="button" onClick={() => navigate("/")} class="backButton">Back</button>
+
             <div class="informationArea">
             <h1>Create Account</h1>
             <h2>Please input information to create an account</h2>
-            
+            <input type="text" placeholder="New Username"></input>
+            <input type="text" placeholder="Full Name"></input>
+            <input type="password" placeholder="New Password"></input>
+            <button type="button" class="createAccountButton">Create Account</button>
             </div>
 
         </div>
