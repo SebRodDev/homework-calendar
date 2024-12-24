56ddd16b2e97e9c66016c1477b53e01ed02169d3
Sebastian Rodriguez <sebastianrodriguez221@hotmail.com>
2024-12-23 20:38:09 -0500

    Starting the backend

---
+++ backend/src/main/java/calendar_homework/Homework/Calendar/HomeworkCalendarApplication.java
@@ -0,0 +1,13 @@
+package calendar_homework.Homework.Calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkCalendarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/calendar_homework/Homework/Calendar/HomeworkCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package calendar_homework.Homework.Calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
