c996f6738efab4710a5a990f8a58dc2e00e70983
Sebastian Rodriguez <sebastianrodriguez221@hotmail.com>
2024-12-30 17:45:48 -0500

    Fixed one of the errors when trying to load up the database

--- backend/src/main/java/calendar_homework/Homework/Calendar/HomeworkCalendarApplication.java
+++ backend/src/main/java/calendar_homework/Homework/Calendar/HomeworkCalendarApplication.java
@@ -2,8 +2,9 @@ package calendar_homework.Homework.Calendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class HomeworkCalendarApplication {
 
 	public static void main(String[] args) {
