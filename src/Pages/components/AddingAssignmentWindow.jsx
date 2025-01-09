95c21fe9062f08ec52431f4bc04da36f514524d3
Sebastian Rodriguez <sebastianrodriguez221@hotmail.com>
2025-01-09 18:24:47 -0500

    progres

---
+++ src/Pages/components/AddingAssignmentWindow.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function AddingAssignmentWindow() {
+    const [dueDate, setDueDate] = useState("");
+    const [assignmentName, setAssignmentName] = useState("");
+    const [className, setClassName] = useState("");
+
+    return (
+        <div>
+            <h3>Please fill out all of the information below: </h3>
+            <input 
+            type="text" 
+            placeholder="Assignment Name"
+            value={assignmentName}
+            onChange={(e) => setAssignmentName(e.target.value)}>
+            </input>
+            <input 
+            type="text" 
+            placeholder="Course Name"
+            value={className}
+            onChange={(e) => setClassName(e.target.value)}>
+            </input>
+            <input 
+            type="text" 
+            placeholder="Due Date"
+            value={dueDate}
+            onChange={(e) => setDueDate(e.target.value)}>
+            </input>
+        </div>
+    )
+}
