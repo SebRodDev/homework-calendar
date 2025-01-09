b76847e29f1158083dfd49e8ad93c55c9a705056
Sebastian Rodriguez <sebastianrodriguez221@hotmail.com>
2025-01-08 23:02:07 -0500

    Further progress

--- src/Pages/HomeworkCalendar.jsx
+++ src/Pages/HomeworkCalendar.jsx
@@ -5,8 +5,14 @@ import "./Styles/HomeworkCalendarLooks.css";
 export default function HomeworkCalendar() {
     const { userUsername } = useParams();
     return (
-        <div>
-            <h1 class="title">Welcome back {userUsername}!</h1>
+        <div class="homeworkBackground">
+            <h1>Homework Calendar</h1>
+            <h2 class="title">Welcome back {userUsername}!</h2>
+            <div>
+                <p>Two options of creating the homework calendar: 
+                    1. Manually input all homework dates to create the homework calendar 
+                    2. Upload a syllabus and allow a machine learning model to create the homework calendar</p>
+            </div>
         </div>
     )
 }
